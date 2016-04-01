a19084e14096876728a0bf743fedc289dafd7d30
ragoomundun raphael <rragoomu@info-s2-14.iut-bm.univ-fcomte.fr>
2016-04-01 10:54:19 +0200

    Ajout fonction userInsert()

--- userInsert.php
+++ userInsert.php
@@ -1,6 +1,12 @@
 <?php include "include/header.php"; ?>
 <?php include "include/menu.php"; ?>
 
-
+<?php
+function userInsert($surname, $address, $datePay)
+{
+	$query = 'INSERT INTO ADHERENT(idAdherent, nomAdherent, adresse, datePaiement)
+	          VALUES(NULL, \'' . $surname . '\', \'' . $address . '\', \'' . $datePay . '\')';
+}
+?>
 
 <?php include "include/footer.php"; ?>
