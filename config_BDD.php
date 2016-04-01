c2583f1cf44a46638f0ccd989a95ddc3aee2a20f
helladan <bahamutzero18>
2016-04-01 10:34:56 +0200

    Ajout du fichier de config de la BDD

---
+++ config_BDD.php
@@ -0,0 +1,25 @@
+<?php
+	// Fichier de configuration de la base de donnée
+	// A remplir et à mettre dans le dossier parent.
+
+	$host = "";
+	$user = "";
+	$pwd = "";
+	$base = "";
+
+	// Config en local
+	if($_SERVER['HTTP_HOST'] == "localhost")
+	{
+		$host = "";
+		$user = "";
+		$pwd = "";
+		$base = "";
+	}
+	// Config en ligne
+	else
+	{
+		$host = "";
+		$user = "";
+		$pwd = "";
+		$base = "";
+	}
