d43e6fddcca2e51845685eb8e26f2743493ce85f
Helladan <anthony.godard@edu.univ-fcomte.fr>
2016-04-01 10:27:43 +0200

    Delete config.php

--- process/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* config.php : définition des constantes nécéssaires à la connexion à la base de donnée */
-
-define('DB_HOST', '');
-define('DB_DATABASE', '');
-define('DB_USER', '');
-define('DB_PASSWORD', '');
-
-?>
