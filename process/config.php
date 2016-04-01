07783f269dcc7b98a3bf1da0f1a2a66f1a26a6c2
ragoomundun raphael <rragoomu@info-s2-14.iut-bm.univ-fcomte.fr>
2016-04-01 10:18:26 +0200

    Ajout dossier de traitement process/

---
+++ process/config.php
@@ -0,0 +1,9 @@
+<?php
+/* config.php : définition des constantes nécéssaires à la connexion à la base de donnée */
+
+define('DB_HOST', '');
+define('DB_DATABASE', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
+
+?>
